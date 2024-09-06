0fcf126e094a12a2754119e5364e68c627438947
Tim HC Lin (林弘佶) <timhc.lin@moxa.com>
2024-09-06 18:53:51 +0800

    Initial version

---
+++ clone.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd ..
+
+git clone git@github.com:timlin1972/center.git
+git clone git@github.com:timlin1972/common.git
+git clone git@github.com:timlin1972/tln_sysinfo.git
---
+++ release.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+DST=../release
+PLUGINS_PATH=plugins
+
+PLUGINS=(
+    "center center ./."
+    "tln_sysinfo libtln_sysinfo.so ./${PLUGINS_PATH}/."
+)
+
+# clean
+rm center
+rm -rf plugins
+
+mkdir -p ${PLUGINS_PATH}
+
+# build
+cd ..
+for PLUGIN_INFO in "${PLUGINS[@]}"; do
+    IFS=' ' read -r -a PLUGIN <<< "$PLUGIN_INFO"
+    SRC_PATH=${PLUGIN[0]}
+    OUTPUT=${PLUGIN[1]}
+    DST_PATH=${PLUGIN[2]}
+
+
+    echo "Processing ${SRC_PATH}..."
+    cd ${SRC_PATH}
+    cargo fmt
+    cargo clippy
+    cargo build --release
+    cp target/release/${OUTPUT} ${DST}/${DST_PATH}
+    cd ..
+done
+
+cd release
+echo "------------------------------"
+./center
